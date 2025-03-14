ac159b38bf99e44fceb41f5f69cfa1567e6c13ab
Abhinav <48094015+AbhinavKumar22@users.noreply.github.com>
2025-03-14 15:22:26 +0530

    Code changes - added footer

--- SWOT_Script.py
+++ SWOT_Script.py
@@ -291,14 +291,29 @@ st.session_state.tokens_consumed = 0
 st.session_state.query_tokens = 0
 st.session_state.response_tokens = 0
 
+st.markdown("""
+    <style>
+        .fixed-footer {
+            position: fixed;
+            left: 0;
+            bottom: 0;
+            width: 100%;
+            background-color: #f1f1f1;
+            color: #555;
+            text-align: center;
+            padding: 10px;
+        }
+    </style>
+    <div class="fixed-footer">
+        Made by Abhinav | Powered by LangChain & Google Gemini Al
+    </div>
+    """, unsafe_allow_html=True)
+
+
 
 # In[ ]:
 
 
-
-
-
-# In[ ]:
 
 
 
